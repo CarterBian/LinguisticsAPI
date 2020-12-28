b684dc1398963f9dcebdaa7633a26a712cb79fdc
Carter Bian <carterbian32@gmail.com>
2020-12-27 20:47:28 -0800

    404

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ CORS(app)
 def index():
     return app.send_static_file('index.html')
 
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
-
 @app.route('/api/add_linguists', methods=['POST'])
 def add_linguists():
 
